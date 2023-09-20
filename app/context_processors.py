206c407bb6e190aef934de5fa4a631fc292464ae
Gabrieleirbag1 <119498688+Gabrieleirbag1@users.noreply.github.com>
2023-09-20 20:16:53 +0200

    Update context_processors.py

--- app/context_processors.py
+++ app/context_processors.py
@@ -3,3 +3,6 @@ from .forms import IdeForm
 def ide_form_context(request):
     form = IdeForm(request.POST or None)
     return {'ide_form': form}
+
+
+#me permet de mettre le formulaire dans le footer de toutes les pages
